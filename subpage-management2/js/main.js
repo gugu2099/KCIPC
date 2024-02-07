946dba0c71f6b1fc53b7065b7b577fd9a39108b3
gugoo2099@gmail.com <gugoo2099@gmail.com>
2024-02-07 14:37:34 +0900

    first

---
+++ subpage-management2/js/main.js
@@ -0,0 +1,36 @@
+window.addEventListener('load', function() {
+    var allElements = document.getElementsByTagName('*');
+    Array.prototype.forEach.call(allElements, function(el) {
+        var includePath = el.dataset.includePath;
+        if (includePath) {
+            var xhttp = new XMLHttpRequest();
+            xhttp.onreadystatechange = function () {
+                if (this.readyState == 4 && this.status == 200) {
+                    el.outerHTML = this.responseText;
+                }
+            };
+            xhttp.open('GET', includePath, true);
+            xhttp.send();
+        }
+    });
+});
+
+// slider start
+// var swiper = new Swiper(".mySwiper", {
+//     spaceBetween: 30,
+//     centeredSlides: true,
+//     loop: true,
+//     autoplay: {
+//         delay: 2500,
+//         disableOnInteraction: false,
+//     },
+//     pagination: {
+//         el: ".swiper-pagination",
+//         clickable: true,
+//     },
+//     navigation: {
+//         nextEl: ".swiper-button-next",
+//         prevEl: ".swiper-button-prev",
+//     },
+// });
+  // slider end
---
+++ subpage-news/js/main.js
@@ -0,0 +1,44 @@
+window.addEventListener('load', function() {
+    var allElements = document.getElementsByTagName('*');
+    Array.prototype.forEach.call(allElements, function(el) {
+        var includePath = el.dataset.includePath;
+        if (includePath) {
+            var xhttp = new XMLHttpRequest();
+            xhttp.onreadystatechange = function () {
+                if (this.readyState == 4 && this.status == 200) {
+                    el.outerHTML = this.responseText;
+                }
+            };
+            xhttp.open('GET', includePath, true);
+            xhttp.send();
+        }
+    });
+});
+
+
+
+// tab start
+const tabItem = document.querySelectorAll(".tab__item");
+const tabContent = document.querySelectorAll(".tab__content");
+
+    tabItem.forEach((item, index) => {
+
+    item.addEventListener("click", (e) => {
+
+        e.preventDefault(); // a 
+        
+
+        tabContent.forEach((content) => {
+
+        content.classList.remove("active");
+        });
+
+        tabItem.forEach((content) => {
+        content.classList.remove("active");
+        });
+
+        tabItem[index].classList.add("active");
+        tabContent[index].classList.add("active");
+    });
+});
+// tab end
