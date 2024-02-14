28b0b3cd98b15243600e49ed1eb81d8408538487
gugoo2099@gmail.com <gugoo2099@gmail.com>
2024-02-14 14:48:01 +0900

    first

--- js/main.js
+++ js/main.js
@@ -14,3 +14,21 @@ window.addEventListener('load', function() {
         }
     });
 });
+
+
+// top start
+$(function() { // 보이기 | 숨기기
+    $(window).scroll(function() {
+    if ($(this).scrollTop() > 250) { //250 넘으면 버튼이 보여짐니다. 
+    $('#topBtn').fadeIn();
+    } else {
+    $('#topBtn').fadeOut();
+    }
+}); // 버튼 클릭시 
+$("#topBtn").click(function() { 
+    $('html, body').animate({ scrollTop : 0 // 0 까지 animation 이동합니다. 
+    }, 600); // 속도 400 
+    return false; 
+}); 
+});
+// top end
