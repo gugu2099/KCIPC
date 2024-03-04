0466d14b5eea86e21601dde7e300b4f9ff531baf
gugoo2099@gmail.com <gugoo2099@gmail.com>
2024-03-04 14:48:05 +0900

    first

--- catalog2/js/main.js
+++ catalog2/js/main.js
@@ -20,10 +20,10 @@ var swiper = new Swiper(".mySwiper", {
     spaceBetween: 30,
     centeredSlides: true,
     loop: true,
-    // autoplay: {
-    //     delay: 2500,
-    //     disableOnInteraction: false,
-    // },
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+    },
     pagination: {
         el: ".swiper-pagination",
         clickable: true,
